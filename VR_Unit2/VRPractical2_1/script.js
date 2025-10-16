c5f087dbc78b7c52708ff7bedbd43794d1bdf610
MrBrandonLin <brandonl385@nycstudents.net>
2025-10-16 16:25:10 +0000

    Just added the house generation.

--- VR_Unit2/VRPractical2_1/script.js
+++ VR_Unit2/VRPractical2_1/script.js
@@ -17,29 +17,13 @@ window.addEventListener("DOMContentLoaded",function() {
     let z = rnd(-20,20);
     createCloud(x,6,z);
   }
-   //Task 2: Use the createCloud(...)  to add several clouds to the scene at various positions.
+  for(let i = 0; i < 100; i++){
+    let x = rnd(-20,20);
+    let z = rnd(-20,20);
+    createHouse(x,0,z);
+  }
 
-   //Task 4: Use the createHouse(...)  to add several houses to the scene at various positions.
-})
 
-/* Task 1: Create a function createCloud that,
-      1) Accept an x, y and z position for where to place the cloud "entity"
-      2) Create an entity to store all the components that will make up the cloud
-      3) Create three sphere, or some other appropriate component that can slightly overlap.         
-      4) Adjust the attributes appropriately.  Add each sphere to the cloud entity
-      5) Set cloud entities position to those passed in to the function.
-      6) Add the cloud entity to the scene
-*/
-
-/* Task 3: Create a function createHouse that,
-      1) Accept an x and z position for where to place the house "entity"
-      2) Create an entity to store all the components that will make up the house
-      3) Create box for the base of the house.  Add base to the entity.
-      4) Create triangular prism from a cylinder for the roof.  Add the roof to the entity.
-      5) Adjust the attributes appropriately.
-      5) Set house entities position to those passed in to the function.
-      6) Add the house entity to the scene
-*/
 function createTree(x, y, z){
   let tree = document.createElement("a-entity");
   
@@ -74,6 +58,37 @@ function createCloud(x, y, z){
   cloud.append( cloud3 );
 
   cloud.setAttribute("position",{x:x, y:y, z:z});
-  cloud.setAttribute("rotation",{x:x, y:y, z:z});
+  cloud.setAttribute("rotation",{x:y, y:z, z:x});
   scene.append( cloud )
 }
+
+function createHouse(x, y, z){
+  let fullhouse = document.createElement("a-entity");
+  
+  let housebase = document.createElement("a-box");
+  housebase.setAttribute("position","0 0 0");
+  housebase.setAttribute("height",".5");
+  housebase.setAttribute("color","orange");
+  fullhouse.append( housebase );
+
+  let housewalls = document.createElement("a-box");
+  housewalls.setAttribute("position","0 .5 0");
+  housewalls.setAttribute("width",".9");
+  housewalls.setAttribute("depth",".9");
+  housebase.setAttribute("height",".5");
+  housewalls.setAttribute("color","yellow");
+  fullhouse.append( housewalls );
+
+  let houseroof = document.createElement("a-cone");
+  houseroof.setAttribute("position","0 1.2 0");
+  houseroof.setAttribute("rotation","0 45 0");
+  houseroof.setAttribute("segments-radial","4");
+  houseroof.setAttribute("height",".5");
+  houseroof.setAttribute("width",".2");
+  houseroof.setAttribute("color","blue");
+  fullhouse.append( houseroof );
+
+  fullhouse.setAttribute("position",{x:x, y:y, z:z});
+  scene.append( fullhouse )
+}
+})
