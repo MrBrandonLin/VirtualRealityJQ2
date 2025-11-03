55df20330acf83d76f00397f1ae01c757cb23d6e
MrBrandonLin <brandonl385@nycstudents.net>
2025-11-03 17:27:04 +0000

    did lesson 2_8 classwork

--- VR_Unit2/VRLesson2_8Classwork/script.js
+++ VR_Unit2/VRLesson2_8Classwork/script.js
@@ -2,13 +2,16 @@ let target;
 
 window.addEventListener("DOMContentLoaded",function() {
   target = document.querySelector("#target");
-  target.a = 0;
-  target.da = -1;
+  target.a = 360;
+  target.da = -.4;
 
   loop();
 })
 
 function loop(){
+  if(target.a <= 270){
+    target.da = 0;
+  }
   target.a += target.da;
 
   target.setAttribute("rotation",{x:target.a, y:0, z: 0});
