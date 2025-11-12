be1a3bd1bda07889fefb64f737db90fff72a5304
MrBrandonLin <brandonl385@nycstudents.net>
2025-11-12 02:05:46 +0000

    just added a block building function to 2_5 practical

---
+++ VR_Unit2/VRPractical2_5/daedalus.js
@@ -0,0 +1,26 @@
+class daedalusparts{
+    constructor(schrodinger, x, y, z){
+        this.x = x;
+        this.y = y;
+        this.z = z; 
+        this.m = 0.2;
+        this.cat = schrodinger;
+        this.wallcom = [], this.wallpar;
+        for(let i = 0; i < 9; i++){
+            this.wallpar = document.createElement("a-box");
+            this.wallpar.setAttribute("position", {x:this.x, y:i, z:this.z});
+            this.wallcom.push(this.wallpar);
+            scene.append(this.wallcom[i])
+        }
+    }
+    observation(){
+        this.secam = document.getElementById("secam");
+        if(this.cat == "true" && (this.secam.object3D.position.x > this.x-5 || this.secam.object3D.position.x < this.x+5) && (this.secam.object3D.position.z > this.z-5 || this.secam.object3D.position.z < this.z+5)){
+            this.y += this.m;
+            for(let i = 0; i < 9; i++){
+                this.wallcom[i].setAttribute("position", {x:this.x, y:this.y, z:this.z});
+            }
+        }
+    }
+}
+
--- VR_Unit2/VRPractical2_5/script.js
+++ VR_Unit2/VRPractical2_5/script.js
@@ -1,9 +1,3 @@
-/* Note
-   Feel free to use classes from past classwork and practical activities.  You may also create new classes.  
-   If you wish to use objects from Unit 1, you can use cloneNode( ) to duplicate them.  As an added bonus you 
-   can also incorporate classes that have animations integrated into them. 
-*/
-
 /* Challenge 1
    Add appropriate classes to use as objects in your maze.  Choose characters to represent these objects and 
    position them in the maze.   In Challenge 3 and 4, you will generate the maze along with any other object 
@@ -33,7 +27,8 @@ let scene;
 
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene");
-  for(let r = 0; r < maze.length; r++){
+
+  //for(let r = 0; r < maze.length; r++){
     /* Challenge 3
       Choose a technique to traverse the each character in the string.
     */ 
@@ -41,6 +36,13 @@ window.addEventListener("DOMContentLoaded",function() {
        Make an appropriate decision based on the characters you chose to enter 
        in the maze.  Create an instance of the corresponding object.
     */
-  }
-
+  //}
+  sexmachine = new daedalusparts("true", 1, -10, -5);
+  loop();
 })
+
+function loop(){
+  sexmachine.observation();
+
+  window.requestAnimationFrame( loop );
+}
