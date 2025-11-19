3776b7315e698ca3401ca0299a80d61524c8f22c
MrBrandonLin <brandonl385@nycstudents.net>
2025-11-19 17:29:28 +0000

    arm is now linked to body

--- BRAdemo/BRAdemoarm/arm.js
+++ BRAdemo/BRAdemoarm/arm.js
@@ -27,21 +27,35 @@ class arm{
         this.arm1.setAttribute("position", "0 3 -2");
         scene.append(this.arm1);
 
-        this.r1 = 0;
-        this.rt1 = 1;
-        this.r2 = 90;
-        this.rt2 = 2;
+        this.r1 = -30;
+        this.rt1 = 1.5;
+        this.r2 = 80;
+        this.rt2 = -2.5;
+        this.curen = 0;
+
     }
     bend(){
-        if(this.r1 < -180 || this.r1 > 0){
-            this.rt1 = -this.rt1;
+        if(this.action == "punch"){
+            if(this.r1 < -90 || this.r1 > -30){
+                this.rt1 = -this.rt1;
+                this.curen += 1;
+            }
+            if(this.r2 < 80 || this.r2 > 180){
+                this.rt2 = -this.rt2;
+            }
+            if(this.curen > 1){
+                this.rt1 = 0, this.rt2 = 0;
+                this.action = "neutral";
+            }
+            this.r1 -= this.rt1;
+            this.r2 -= this.rt2;
+            console.log(this.curen);
         }
-        if(this.r2 < 45 || this.r2 > 180){
-            this.rt2 = -this.rt2;
-        }
-        this.r1 -= this.rt1;
-        this.r2 -= this.rt2;
-        this.arm1.setAttribute("rotation", {x:0, y:0, z:this.r1});
+        this.arm1.setAttribute("rotation", {x:0, y:270, z:this.r1});
         this.arm2.setAttribute("rotation", {x:0, y:0, z:this.r2});
+    }
+    follow(){
+        this.secam = document.getElementById("secam");
+        this.arm1.setAttribute("position", {x:this.secam.object3D.position.x+1,y:3,z:this.secam.object3D.position.z-.5});
     }
 }
--- BRAdemo/BRAdemoarm/script.js
+++ BRAdemo/BRAdemoarm/script.js
@@ -1,10 +1,19 @@
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene");
   heavensgrace = new arm();
+  window.addEventListener("click", function(){
+    heavensgrace.action = "punch";
+    heavensgrace.curen = 0;
+    heavensgrace.r1 = -30;
+    heavensgrace.rt1 = 1.5;
+    heavensgrace.r2 = 80;
+    heavensgrace.rt2 = -2.5;
+  })
   natural();
 })
 
 function natural(){
   heavensgrace.bend();
+  heavensgrace.follow();
   window.requestAnimationFrame( natural );
 }
