5e2b512cc22e770e888d6ad20ef746cd771ff4a4
MrBrandonLin <brandonl385@nycstudents.net>
2025-12-20 18:51:39 +0000

    music time

--- EscapingReality/rhythm.js
+++ EscapingReality/rhythm.js
@@ -1,20 +1,43 @@
 class timer{
     constructor(){
-        this.song = ["1L1R1L2R"]
+        this.beatbox = document.createElement("a-box");
+        this.beatbox.setAttribute("sound", {src:"#Song1", loop:true, refDistance:1000})
+        this.song = ["pPpPpPpPpPpPpPpPpPpPpPpPpPpPpPpPpPpPpPpP",
+                     "pPpPpPpPpPpPpPpPpPpPpPpP1LpPpPpPpPpPpPpP",
+                     "NNNNNNNNNNNNNNNNNNNNNNNNNNNNN",
+                     "PPPPPPPPPPPPPPPPPPPPPPPP",
+                     "PPPPPPPPPPPPPPPPPPPPPPPP",
+        ];
         this.songtime = 0;
+        this.linetime = 0;
         this.s = 0;
+        this.play = false;
+
+        this.beatbox.addEventListener("click", ()=>{
+            this.play = true;
+        })
+        scene.append(this.beatbox);
     }
     timering(){
-        if(this.song[this.songtime*2]=="1L"){
-            console.log("YEAH");
-        }
-        this.s+=.25;
-        if(this.s%.5 == 0){
-            console.log("another half second passes");
-        }
-        if(this.s%1 == 0){
-            console.log("another second passes");
-        }
         console.log(this.s);
+        if(this.linetime>=this.song.length){
+                this.play = false;
+        }
+        if(this.play){
+            this.beatbox.components.sound.playSound();
+            this.s+=.25;
+            this.songtime+=1;
+            if(this.songtime>this.song[this.linetime].length/2){
+                this.songtime = 0;
+                this.linetime += 1;
+            }
+            console.log(this.song[this.linetime].substring(this.songtime*2, (this.songtime*2)+2));
+            if(this.s%.5 == 0){
+            }
+            if(this.s%1 == 0){
+            }
+        } else if(this.play != true){
+            this.beatbox.components.sound.stopSound();
+        }
     }
 }
